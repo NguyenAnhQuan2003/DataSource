4e079284876803f058c601db8e5ef909810fd24d
nguyenanhquan <quan715105194@gmail.com>
2025-08-25 23:32:29 +0700

    Update pipeline

--- tasks/read_ips.py
+++ tasks/read_ips.py
@@ -15,15 +15,7 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 def unique_read_ips(limit=None):
     db = connect_mongodb()
     src_col = db[collection_]
-    # dst_col = db[collection_ip_data]
     ip2loc = IP2Location.IP2Location("/home/decquannguyen/IP-COUNTRY-REGION-CITY.BIN")
-    # r = redis.Redis(host="localhost", port=6379, db=0)
-    # query = src_col.find({}, {"ip": 1})
-    # if limit:
-    #     query = query.limit(limit)
-    #
-    # buffer = []
-    # count = 0
     pipeline = [
         {"$group": {"_id": "$ip"}}
     ]
@@ -33,41 +25,6 @@ def unique_read_ips(limit=None):
     cursor = src_col.aggregate(pipeline, allowDiskUse=True)
     buffer = []
     file_count = 1
-    # for i, d in enumerate(query, start=1):
-    #     ip = d.get("ip")
-    #     if not ip:
-    #         continue
-    #
-    #     try:
-    #         cached = r.get(ip)
-    #         if cached:
-    #             rec = eval(cached.decode())
-    #         else:
-    #             lookup = ip2loc.get_all(ip)
-    #             rec = {
-    #                 "country_short": lookup.country_short,
-    #                 "country_long": lookup.country_long,
-    #                 "region": lookup.region,
-    #                 "city": lookup.city,
-    #             }
-    #             r.set(ip, str(rec))
-    #
-    #         new_doc = {"ip": ip, **rec}
-    #         buffer.append(new_doc)
-    #
-    #     except Exception as e:
-    #         logging.error(f"[{i}] Lỗi enrich IP {ip}: {e}")
-    #
-    #     if len(buffer) >= batch_size:
-    #         dst_col.insert_many(buffer)
-    #         count += len(buffer)
-    #         logging.info(f"Đã insert {count} documents...")
-    #         buffer.clear()
-    #
-    # if buffer:
-    #     dst_col.insert_many(buffer)
-    #     count += len(buffer)
-    #     logging.info(f"Hoàn tất insert {count} documents.")
     for i, doc in enumerate(cursor, start=1):
         ip = doc["_id"]
         if not ip:
